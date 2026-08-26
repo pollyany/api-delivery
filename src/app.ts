a0f8567615bb204d6250e1b2ef071362f65603c5
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-26 15:52:41 -0300

    feat: add user routes and controller implementation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import 'express-async-errors';
-import { errorHandling } from './middleware/error-handling';
+import express from "express";
+import "express-async-errors";
+import { errorHandling } from "./middleware/error-handling";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-app.use(errorHandling)
+app.use(routes);
+app.use(errorHandling);
 
 export { app };
