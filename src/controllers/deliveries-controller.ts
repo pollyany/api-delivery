7a29bd811ee1eb677f5904b538a5474c211cd847
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-27 15:33:28 -0300

    feat: implement delivery creation logic in DeliveriesController

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,9 +1,25 @@
-import { Request, Response } from 'express';
+import { prisma } from "@/database/prisma";
+import { Request, Response } from "express";
+import z from "zod";
 
 class DeliveriesController {
-    create(request: Request, response: Response) {
-        return response.json({ message: 'Delivery created successfully' });
-    }
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json({ message: "Delivery created successfully" });
+  }
 }
 
 export { DeliveriesController };
