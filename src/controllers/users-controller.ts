bf2cc045507fd4d12635e389fe3adfc610006c24
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-27 13:13:22 -0300

    feat: add user creation with password hashing and email validation

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,41 @@
 import { Request, Response } from "express";
-
+import { hash } from "bcrypt";
+import { z } from "zod";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const userWithSameEmail = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists", 409);
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.json(userWithoutPassword);
   }
 }
 
