2865859a68fb9c6d076a7ba2d6394ae28d2b9068
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-27 15:21:34 -0300

    feat: add user authorization middleware to deliveries routes

--- src/routes/deliveries-route.ts
+++ src/routes/deliveries-route.ts
@@ -1,11 +1,12 @@
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { ensureAuthenticated } from "@/middleware/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middleware/verify-user-authorization";
 import { Router } from "express";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
-deliveriesRoutes.use(ensureAuthenticated);
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
 
 export { deliveriesRoutes };
